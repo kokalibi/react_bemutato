88ba56b7c5860f1d477a1dead7ce20d831e40530
soczo <soczo.levente.711@gmail.com>
2025-04-01 12:24:23 +0200

    galeria

--- src/Components/GaleriaReact.jsx
+++ src/Components/GaleriaReact.jsx
@@ -1,4 +1,45 @@
-export default function Gallery(){
-    <></>
+import { useState } from "react";
+import { GalleryList } from "../data/galeria";
 
+export default function Gallery() {
+    const [index, setIndex] = useState(0);
+
+    function handleNextClick() {
+        if (index < GalleryList.length - 1) {
+            setIndex(index + 1);
+        } else {
+            setIndex(0);
+        }
+    }
+
+    function handlePrevClick() {
+        if (index > 0) {
+            setIndex(index - 1);
+        } else {
+            setIndex(GalleryList.length - 1);
+        }
+    }
+
+    const galleryItem = GalleryList[index];
+
+    return (
+        
+        <>
+            <h2>Galeria - {index + 1}/{GalleryList.length}. Elem</h2>
+            <button onClick={handlePrevClick}>⬅️</button>
+            <button onClick={handleNextClick}>➡️</button>
+
+            <h2>
+                {galleryItem.name} by {galleryItem.artist}
+            </h2>
+            <div>
+                {galleryItem.description}
+                <img 
+                    src={galleryItem.url} 
+                    alt={galleryItem.alt} 
+                    style={{ width: "500px", height: "500px" }}
+                />
+            </div>
+        </>
+    );
 }
---
+++ src/data/galeria.js
@@ -0,0 +1,26 @@
+export const GalleryList = [{
+    name: 'Puente Nuevo',
+    description: 'The obvious starting point for any visit to Ronda is the Puente Nuevo (new bridge) – easily the most recognisable landmark in Ronda. The cobbled bridge spans El Tajo gorge between the old and new towns and, despite being called the new bridge, the Puente Nuevo is actually over 200 years old, having been completed in 1793 after over 40 years in the making.',
+    url: 'https://alisononfoot.com/wp-content/uploads/2024/07/Things-to-do-in-Ronda2-2048x1470.jpg',
+    alt: 'Puente Nuevo'
+  },{
+    name: 'Casa Museo don Bosco',
+    description: 'Originally the house was a private mansion. In 1939 it was donated to the local Catholic Salesian congregation. Until 2008 the house was mainly used as a sanatorium for members of said religious order.',
+    url: 'https://casamuseodonbosco.com/wp-content/uploads/2023/01/casa-museo-don-bosco-ronda-1024x1024.jpg',
+    alt: 'Casa Museo don Bosco'
+  },{
+    name: 'El Tajo Gorge',
+    description: 'This space, declared an Andalusian Natural Monument (Monumento Natural de Andalucía in 2019), has two distinct sections. The first, a 500-metre long gorge that is 100 metre deep that goes to the Guadalevín river. The other part is a large scarp that opens onto “La Caldera”, a circular hollow.',
+    url: 'https://static.costadelsolmalaga.org/visita/subidas/imagenes/3/1/arc_19513_g.png',
+    alt: 'El Tajo Gorge'
+  },{
+    name: 'La Ciudad',
+    description: 'Wander through Ronda’s ancient streets, where you’ll find narrow, winding alleys, whitewashed buildings, and charming squares. The old town offers a glimpse into Ronda’s Moorish and Christian history.',
+    url: 'https://multimedia.andalucia.org/fotos/image_151672.jpeg',
+    alt: 'La Ciudad'
+  },{
+    name: 'Alameda del Tajo Park',
+    description: 'This beautiful park offers stunning views of the El Tajo Gorge and the surrounding countryside. Its a peaceful place to take a stroll and relax in nature, with fountains, sculptures, and plenty of greenery.',
+    url: 'https://www.andalucia.com/sites/default/files/MVC222850078-Ronda-%20Andalucia-Spain-copyright-m-chaplow.jpg',
+    alt: 'Alameda del Tajo Park'
+  }]
