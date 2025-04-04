e48bbf934304514e1d7d256b093a3d45ada9c114
kokalibi <balint00istvan@gmail.com>
2025-04-04 07:48:48 +0200

    :P

--- src/Components/TerkepReact.jsx
+++ src/Components/TerkepReact.jsx
@@ -1,12 +1,32 @@
-export default function Terkep(){
-    return(
-        <>
-<img 
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzPD-UdIDzD7tkY2yGKlx0DMETfVQ3Yu85_Q&s" 
-        alt="" 
-        style={{ width: '100%', height: 'auto', borderRadius: '10px'   }} 
-      />
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
-        </>
-            )
+export default function Terkep() {
+    // Ronda koordinátái (Spanyolország)
+    const rondaCoords = [36.7428, -5.1628];
+
+    // Egyéni ikon a markerhez
+    const customIcon = new L.Icon({
+        iconUrl: '/pngwing.com.png',
+        iconSize: [40, 40], // Az ikon méretének beállítása
+        iconAnchor: [20, 40], // Az ikon alapjának a pozicionálása
+        popupAnchor: [0, -40], // A felugró ablak pozicionálása
+    });
+
+    return (
+        <div style={{ height: '500px' }}>
+            <MapContainer center={rondaCoords} zoom={13} style={{ width: '100%', height: '100%' }}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={rondaCoords} icon={customIcon}>
+                    <Popup>
+                        Ronda, Spanyolország
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    );
 }
